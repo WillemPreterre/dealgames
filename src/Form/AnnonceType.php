48b286dde209489155935618dbb5f1a20ab616c7
WillemPreterre <wpreterre.lamanu@gmail.com>
2022-04-11 15:47:08 +0200

    filrouge2

---
+++ src/Form/AnnonceType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Annonce;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AnnonceType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('annonce_title')
+            ->add('annonce_description')
+            ->add('annonce_date')
+            ->add('annonce_date_modif')
+            ->add('annonce_date_delete')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Annonce::class,
+        ]);
+    }
+}

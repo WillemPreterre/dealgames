7590101c1122bf1a7fae0cf700598da42947f6af
WillemPreterre <wpreterre.lamanu@gmail.com>
2022-04-19 16:46:16 +0200

    login + reset + validation email

--- src/Controller/AnnonceController.php
+++ src/Controller/AnnonceController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/annonce")
+ * @Route("/profile/annonce")
  */
 class AnnonceController extends AbstractController
 {
